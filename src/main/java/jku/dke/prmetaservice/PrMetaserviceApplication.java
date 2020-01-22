8142073cd5b57c21467f775e99a5463644ecded6
chrigHub <51883441+chrigHub@users.noreply.github.com>
2020-01-22 17:50:24 +0100

    Initial commit

---
+++ src/main/java/jku/dke/prmetaservice/PrMetaserviceApplication.java
@@ -0,0 +1,13 @@
+package jku.dke.prmetaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrMetaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrMetaserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jku/dke/prmetaservice/PrMetaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package jku.dke.prmetaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrMetaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
