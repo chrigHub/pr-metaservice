eb1c2674565a0bc5a253489e017dbc5523435778
Felix Winterleitner <f.winterleitner@blitzkabine.com>
2020-01-27 17:15:56 +0100

    first impl of form

--- src/main/java/jku/dke/prmetaservice/controller/IndexController.java
+++ src/main/java/jku/dke/prmetaservice/controller/IndexController.java
@@ -10,17 +10,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.*;
 
 @Controller
 public class IndexController {
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
     private SparqlServiceImpl sparqlService = new SparqlServiceImpl();
-    private Set<String> brandSet = new HashSet<String>();
+    private Set<String> brandSet = new HashSet<>();
+    private Set<String> modelSet = new HashSet<>();
+    private SparqlTriple triple = new SparqlTriple();
+
+
+    private String brand_choice;
+    private String model_choice;
 
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("sparqlTriple", new SparqlTriple());
         this.sparqlService.setEndpoint("http://localhost:3030//");
 
         this.sparqlService.getBrandsFromAudi().forEach(row -> {
@@ -38,7 +45,12 @@ public class IndexController {
                 this.brandSet.add(entry);
             });
         });
+
+        model.addAttribute("sparql_triple", this.triple);
         model.addAttribute("brands", this.brandSet);
+        model.addAttribute("brand_choice", this.brand_choice);
+        model.addAttribute("model", this.modelSet);
+        model.addAttribute("model_choice", this.model_choice);
         log.info(this.brandSet.toString());
         log.info("Entered get index");
         return "index";
@@ -52,6 +64,17 @@ public class IndexController {
         resultList = sparqlService.getAllTriples(endpoint);
         model.addAttribute("resultList", resultList);
         log.info(resultList.toString());
+        return "index";
+    }
+
+    @PostMapping("/selectBrand")
+    public String selectBrand (@RequestParam(name="brand") String brand, Model model){
+        this.brand_choice = brand;
+        model.addAttribute("sparql_triple", this.triple);
+        model.addAttribute("brands", this.brandSet);
+        model.addAttribute("brand_choice", this.brand_choice);
+        model.addAttribute("model", this.modelSet);
+        model.addAttribute("model_choice", this.model_choice);
         return "index";
     }
 }
